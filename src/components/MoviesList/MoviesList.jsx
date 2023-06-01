12678f95770039b3b2ad2835b20ce37df55d9acb
VolodymyrK2 <tema.rivne@gmail.com>
2023-06-01 23:52:16 +0300

    moviesList fixed

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import css from './MoviesList.module.css'
 function MoviesList({ moviesList, location }) {
   return (
-    <ul>
+    <ul className={css.movies__list}>
             {moviesList?.map((movie) => (
                 <li key={movie.id}>
                     <Link to={`/movies/${movie.id}`} state={{ from: location }}>{movie.title ?? movie.name}</Link>
